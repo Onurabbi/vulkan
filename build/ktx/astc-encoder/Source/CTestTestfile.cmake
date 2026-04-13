c13a5a384279e706f31a55dc53018a382a76dca3
Onur Gules <onurgules2@gmail.com>
2026-04-13 20:26:37 +0100

    removing build

--- build/ktx/astc-encoder/Source/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/onurabbi/projects/workspace/vulkan/extern/KTX-Software/external/astc-encoder/Source
-# Build directory: /home/onurabbi/projects/workspace/vulkan/build/ktx/astc-encoder/Source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
